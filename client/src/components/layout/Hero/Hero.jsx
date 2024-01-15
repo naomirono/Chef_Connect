3e503271fc6855d7a8c8365d5fa55c86a566955a
Naomi Cherotich <naomirono1454@gmail.com>
2024-01-15 21:12:10 +0300

    Add a url for background image and adjust the height

--- client/src/components/layout/Hero/Hero.jsx
+++ client/src/components/layout/Hero/Hero.jsx
@@ -1,10 +1,10 @@
 const Hero = () => {
   return (
     <section
-      id="about"
-      className="py-8 mx-auto items-center relative h-[90vh] 2xl:h-[80vh]"
+      id="hero"
+      className="py-8 mx-auto mt-20 items-center relative h-full 2xl:h-[80vh]"
       style={{
-        backgroundImage: "url('/src/assets/catering1.jpg')",
+        backgroundImage: "url('https://chefconnectapp.s3.ap-south-1.amazonaws.com/catering1.jpg')",
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }}
@@ -46,12 +46,12 @@ const Hero = () => {
           <div className="md:w-1/2 pb-10 md:pb-36 lg:block">
             <div className="relative">
               <img
-                src="/src/assets/chef2.jpg"
+                src="https://chefconnectapp.s3.ap-south-1.amazonaws.com/chef2.jpg"
                 alt="Image"
                 className="w-[250px] md:w-5/6 h-[300px] md:h-[500px] lg:h-[500px] md:max-w-md ml-[80px] border-2 border-orange-500"
               />
               <img
-                src="/src/assets/Chefbg9.png"
+                src="https://chefconnectapp.s3.ap-south-1.amazonaws.com/Chefbg9.png"
                 alt="Image"
                 className="w-[180px] h-[180px] md:w-5/6 absolute bottom-[-40px] md:bottom-[25px] left-[-40px] md:left-[-100px] md:mr-[-10%] md:mb-[-17%]"
               />
@@ -59,55 +59,56 @@ const Hero = () => {
           </div>
         </div>
       </div>
-
-      <div className="flex items-center justify-center z-10 absolute bottom-[-65px] 2xl:bottom-[-64px] right-[250px] 2xl:right-[380px] hidden lg:block">
+    <div className="relative">
+      <div className="flex items-center justify-center z-10 absolute bottom-[-90px] 2xl:bottom-[-64px] right-[250px] 2xl:right-[380px] hidden lg:block ">
         <div className="bg-white bg-opacity-40 backdrop-filter backdrop-blur-l px-4 pt-4 pb-6 rounded shadow-lg max-w-[1100px] mx-auto">
           <h2 className="text-xl md:text-2xl lg:text-3xl font-bold mb-4">Chef Connect</h2>
 
-    {/* Filters */}
-    <div className="flex items-center space-x-4">
-      <select
-        className=" px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-white bg-opacity-40 backdrop-filter backdrop-blur-l"
-      >
-        <option value="">Select Cuisine</option>
-        <option value="nyama-choma">Nyama Choma</option>
-        <option value="ugali-fish">Ugali and Fish</option>
-        <option value="sukuma-wiki">Sukuma Wiki</option>
-      </select>
+         {/* Filters */}
+         <div className="flex items-center space-x-4">
+           <select
+             className=" px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-white bg-opacity-40 backdrop-filter backdrop-blur-l"
+           >
+             <option value="">Select Cuisine</option>
+             <option value="nyama-choma">Nyama Choma</option>
+             <option value="ugali-fish">Ugali and Fish</option>
+             <option value="sukuma-wiki">Sukuma Wiki</option>
+           </select>
 
-      <select
-        className=" px-4 py-2 rounded-md focus:outline-none focus:border-orange-500 bg-white bg-opacity-40 backdrop-filter backdrop-blur-l"
-      >
-        <option value="">Select Rating</option>
-        <option value="5">5 Stars</option>
-        <option value="4">4 Stars</option>
-        <option value="3">3 Stars</option>
-      </select>
+           <select
+             className=" px-4 py-2 rounded-md focus:outline-none focus:border-orange-500 bg-white bg-opacity-40 backdrop-filter backdrop-blur-l"
+           >
+             <option value="">Select Rating</option>
+             <option value="5">5 Stars</option>
+             <option value="4">4 Stars</option>
+             <option value="3">3 Stars</option>
+           </select>
 
-      <select
-        className=" px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-white bg-opacity-40 backdrop-filter backdrop-blur-l"
-      >
-        <option value="">Select Availability</option>
-        <option value="lunch">Lunch</option>
-        <option value="dinner">Dinner</option>
-        <option value="weekends">Weekends</option>
-      </select>
+           <select
+             className=" px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-white bg-opacity-40 backdrop-filter backdrop-blur-l"
+           >
+             <option value="">Select Availability</option>
+             <option value="lunch">Lunch</option>
+             <option value="dinner">Dinner</option>
+             <option value="weekends">Weekends</option>
+           </select>
 
-      <input
-        type="text"
-        placeholder="Enter Location"
-        className="placeholder:text-black px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-white bg-opacity-40 backdrop-filter backdrop-blur-l"
-      />
+           <input
+             type="text"
+             placeholder="Enter Location"
+             className="placeholder:text-black px-4 py-2 rounded-md focus:outline-none focus:border-blue-500 bg-white bg-opacity-40 backdrop-filter backdrop-blur-l"
+           />
 
-      {/* Get Started Button */}
-      <button className="bg-orange-500 text-white py-2 px-6 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
-        Search
-      </button>
-    </div>
+           {/* Get Started Button */}
+           <button className="bg-orange-500 text-white py-2 px-6 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
+             Search
+           </button>
+         </div>
     
-  </div>
-</div>
-
+        </div>
+      </div>
+      </div>
+    
 
     </section>
   );
