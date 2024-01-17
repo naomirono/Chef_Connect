1b388e4d93c14c9dfe23c04fc01577073bfae423
Naomi Cherotich <naomirono1454@gmail.com>
2024-01-17 20:14:14 +0300

    Change the cors origin to deployed url website

--- api/server.js
+++ api/server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://chef-connect-neyo.web.app"],
     credentials: true,
   })
 );
