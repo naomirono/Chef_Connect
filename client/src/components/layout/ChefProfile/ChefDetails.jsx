173d38b15d01d59bb108710d99d10288cc3e932e
Naomi Cherotich <naomirono1454@gmail.com>
2024-01-19 20:17:45 +0300

    Update tailwind styling

--- client/src/components/layout/ChefProfile/ChefDetails.jsx
+++ client/src/components/layout/ChefProfile/ChefDetails.jsx
@@ -41,7 +41,7 @@ const ChefDetails = () => {
   
 
   return (
-    <div className="max-w-[1000px] mx-auto h-[50vh] mt-24 bg-white shadow-md rounded-md ">
+    <div className="max-w-[1000px] mx-auto h-[50vh] mt-24 shadow-md rounded-md ">
      {chefDetails.length > 0 ? (
        chefDetails.map((chef) => (
       
@@ -51,20 +51,28 @@ const ChefDetails = () => {
           <img
             src={chef.cuisineImage}
             alt="Cuisine"
-            className="w-3/4 h-3/4"
+            className="w-3/4 h-3/4 z-20"
           />
+          <div className="absolute top-[-170px] right-72 w-full h-full">
+      
+      <img
+        src="https://chefconnectapp.s3.ap-south-1.amazonaws.com/Splash2.svg"  
+        alt="Splash Overlay"
+        className="w-full h-full splashSvg"
+      />
+    </div>
         </div>
         {/* Right Section: Cuisine Details */}
         <div className="md:w-1/2 lg:block">
-          <h2 className="text-orange-500 text-xl font-bold mb-2">
+          <h2 className="text-orange-500 text-2xl font-extrabold font-fira mb-2">
             {chef.cuisine}
           </h2>
-          <p className="text-gray-900 mb-4 p-2">{chef.description}</p>
+          <p className="text-gray-600 mb-4 p-2 font-inter">{chef.description}</p>
           <div className="flex items-center mb-4">
-            <p className="text-gray-900 mr-4"><i className="fa-solid fa-people-group text-orange-500"></i> Serving - {chef.servings} </p>
-            <p className="text-gray-900"><i className="fa-solid fa-kitchen-set icon text-orange-500"></i> Cook Time - {chef.cookTime}</p>
+            <p className="text-gray-600 mr-8"><i className="fa-solid fa-people-group text-orange-500 mr-2"></i> Serving - {chef.servings} </p>
+            <p className="text-gray-600 ml-4"><i className="fa-solid fa-kitchen-set icon text-orange-500 mr-2"></i> Cook Time - {chef.cookTime}</p>
           </div>
-          <button className="bg-orange-500 text-white py-2 px-6 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
+          <button className="bg-orange-500 text-white mt-4 py-1.5 px-5 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
             Book Now
           </button>
           {/* Chef Details */}
@@ -72,11 +80,11 @@ const ChefDetails = () => {
             <img
               src={chef.profileImage}
               alt={`Chef: ${chef.name}`}
-              className="w-10 h-10 rounded-full mr-4"
+              className="w-10 h-10 rounded-full border border-orange-500 mr-4"
             />
             <div>
-              <p className="text-gray-900 font-bold">{chef.name}</p>
-              <p className="text-gray-900">{chef.location}</p>
+              <p className="text-gray-600 font-bold">{chef.name}</p>
+              <p className="text-gray-600">{chef.location}</p>
             </div>
           </div>
         </div>
