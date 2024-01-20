6e88bde89bb4b39075f8bfc54c4dfd22b647cd62
Naomi Cherotich <naomirono1454@gmail.com>
2024-01-20 19:55:06 +0300

    update API base URL and credentials

--- client/src/features/slices/apiSlice.js
+++ client/src/features/slices/apiSlice.js
@@ -1,9 +1,12 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "" });
+const baseQuery = fetchBaseQuery({
+  baseUrl: "https://us-central1-chef-connect-neyo.cloudfunctions.net/app",
+  credentials: "include",
+});
 
 export const apiSlice = createApi({
   baseQuery,
   tagTypes: ["User"],
-  endpoints: (builder) => ({}),
+  endpoints: () => ({}),
 });
