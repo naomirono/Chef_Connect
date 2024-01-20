f05ece0070c1f3368e7bfa9ac1d4bc200337d1ab
Naomi Cherotich <naomirono1454@gmail.com>
2024-01-20 19:21:00 +0300

    Change the images to use the hosted URL

--- client/src/components/layout/Featured/FeaturedData.js
+++ client/src/components/layout/Featured/FeaturedData.js
@@ -3,24 +3,24 @@ export const chefs = [
       name: 'Chef John Doe',
       expertise: 'Italian Cuisine',
       location: 'Mombasa, Kenya',
-      imageSrc: '/src/assets/chef2.jpg',
+      imageSrc: 'https://chefconnectapp.s3.ap-south-1.amazonaws.com/chef2.jpg',
     },
     {
       name: 'Chef Jane Smith',
       expertise: 'Asian Fusion',
       location: 'Nairobi, Kenya',
-      imageSrc: '/src/assets/CateringF1.jpg',
+      imageSrc: 'https://chefconnectapp.s3.ap-south-1.amazonaws.com/CateringF1.jpg',
     },
     {
         name: 'Chef John Doe',
         expertise: 'Swahili Cuisine',
         location: 'Mombasa, Kenya',
-        imageSrc: '/src/assets/CateringF2.jpg',
+        imageSrc: 'https://chefconnectapp.s3.ap-south-1.amazonaws.com/CateringF2.jpg',
       },
       {
         name: 'Chef Jane Smith',
         expertise: 'Kikuyu Delicacies',
         location: 'Nakuru, Kenya',
-        imageSrc: '/src/assets/chef2.jpg',
+        imageSrc: 'https://chefconnectapp.s3.ap-south-1.amazonaws.com/chef2.jpg',
       }
   ];
