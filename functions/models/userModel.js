b232b5488cc494b670d6c38066c47e4da2a4ddbb
Naomi Cherotich <naomirono1454@gmail.com>
2024-01-21 23:39:02 +0300

    feat: Implement User model with password hashing

---
+++ functions/models/userModel.js
@@ -0,0 +1,86 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      unique: true,
+      trim: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please enter a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      minLength: [6, "Password must be at least 6 characters"],
+      trim: true,
+    },
+    phoneNumber: {
+      type: Number,
+    },
+    addresses: [
+      {
+        country: {
+          type: String,
+        },
+        city: {
+          type: String,
+        },
+        address1: {
+          type: String,
+        },
+        address2: {
+          type: String,
+        },
+        zipCode: {
+          type: Number,
+        },
+        addressType: {
+          type: String,
+        },
+      },
+    ],
+    role: {
+      type: String,
+      default: "user",
+    },
+    imageURL: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/dk7mw2ypf/image/upload/v1695389021/lehuunzchy18tka78re3.png",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    resetPasswordToken: String,
+    resetPasswordTime: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Hash Password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(12);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Compare password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
